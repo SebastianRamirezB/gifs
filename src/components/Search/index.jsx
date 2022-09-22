7373794e385e309798b001a9b2969fb775a3d695
Sebastian Camilo Ramirez Betancur <sebastianramirez748@gmail.com>
2022-09-21 23:09:47 -0500

    logic search component

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
 import { Form, Input } from './styles';
 
 export const Search = () => {
+
+const [inputSearch, setInputSearch] = useState({
+    typing: false,
+    text: ''
+});
+
+
+
+  const handleChange = (e) => {
+    setInputSearch({
+        typing: true,
+        text: e.target.value
+    });
+
+  }
+
+
+
   return (
     <Form>
-        <Input type="text" />
+        <Input 
+            type="text"
+            onChange={handleChange} 
+        />
     </Form>
   )
 }
